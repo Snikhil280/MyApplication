405eb79d1d7372f5239129bc1b28e02bc105220c
qburst <nikhil.k@qburst.com>
2022-02-28 11:08:43 +0530

    AppCenter connections

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        AppCenter.start(
+            application, "ed4c5f95-53aa-40d8-8568-63dc98063f0c",
+            Analytics::class.java, Crashes::class.java
+        )
     }
 }
